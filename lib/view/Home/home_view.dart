a11d172368065642bf39894e35e31f2edecdd14e
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-09-21 15:33:36 +0530

    change

--- lib/view/Home/home_view.dart
+++ lib/view/Home/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -8,6 +10,23 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  getuserdata(){
+    try{
+      firestore.collection('client data').where("age",isLessThan: 20).get().then((value) => {
+        debugPrint("")
+      },);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getuserdata();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
